e6491fe320fe07e34cb706862acd1c0c527eab87
mac-mini <lexx.mg@gmail.com>
2020-11-13 19:45:01 +0300

    add button

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ const firstMessage = document.getElementById('msg_window').querySelector('.msg_r
 const inputMessege = document.getElementById('message');
 const btnSendMessage = document.getElementById('button_send_submit');
 
+const btn = document.createElement('button');
+btn.innerHTML = '<b>Отправить сообщение</b>';
+document.getElementById('composer').append(btn);
 
 function sendMessage(message, callback) {
   const firstMessage = document.getElementById('msg_window').querySelector('.msg_row');
@@ -20,10 +23,17 @@ function sendMessage(message, callback) {
     setTimeout(() => {
       btnSendMessage.click();
       callback('Сообщение отправлено');
-      window.history.back(-1);
-    }, 1000);
+    }, 2000);
   }
 }
 
+btn.addEventListener('click', () => {
+  sendMessage('Привет', (ms) => {
+    console.log(ms);
+    setTimeout(() => {
+      window.history.back(-1);
+    }, 2000);
+  });
+});
 
 //sendMessage('Привет', (ms) => {console.log(ms)});
